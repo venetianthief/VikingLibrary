e7445811f7f01025d3fb5cc0e5dd7222037ec885
Matt Avinger <matt.avinger@gmail.com>
2014-09-06 05:54:19 -0400

    Added function NumberToHuman from VikingUnitFrames.

--- VikingLibrary.lua
+++ VikingLibrary.lua
@@ -5,10 +5,9 @@ require "GameLib"
 -- require "Window"
 -- require "ChatSystemLib"
 
------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- VikingLibrary Module Definition
------------------------------------------------------------------------------------------------
-
+--------------------------------------------------------------------------------
 local VikingLibrary = {
   _VERSION = 'VikingLibrary.lua 0.0.1',
   _URL     = 'https://github.com/vikinghug/VikingLibrary',
@@ -43,23 +42,25 @@ local VikingLibrary = {
 
 local tModules = {}
 
------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- Constants
------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- Initialization
------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 function VikingLibrary:new(o)
     o = o or {}
     setmetatable(o, self)
     self.__index = self
 
-
     return o
 end
 
 
+--------------------------------------------------------------------------------
+--
+--------------------------------------------------------------------------------
 function VikingLibrary:Init()
   local bHasConfigureFunction = false
   local strConfigureButtonText = ""
@@ -69,9 +70,9 @@ function VikingLibrary:Init()
 end
 
 
------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- VikingLibrary OnLoad
------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 function VikingLibrary:OnLoad()
   -- load our form file
   self.xmlDoc = XmlDoc.CreateFromFile("VikingLibrary.xml")
@@ -81,25 +82,35 @@ function VikingLibrary:OnLoad()
 
 end
 
------------------------------------------------------------------------------------------------
--- Public Methods
------------------------------------------------------------------------------------------------
 
+--------------------------------------------------------------------------------
+-- Public Methods: START
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
 -- Assumes that alpha is a value of 0-100
+--------------------------------------------------------------------------------
 function VikingLibrary:NewColor(color, alpha)
   self.color(color, alpha)
   return ApolloColor.new(sAlpha .. self.tColors[color])
 end
 
+
+--------------------------------------------------------------------------------
+-- Is this part of VikingLibrary:NewColor?
+--------------------------------------------------------------------------------
 function VikingLibrary.color(color, alpha)
   alpha = alpha == nil and 100
   local sAlpha = string.format("%x", (alpha / 100) * 255)
   return sAlpha .. VikingLibrary.tColors[color]
 end
 
+
+--------------------------------------------------------------------------------
 -- GetKeyFromValue(t, value)
 --
 -- Finds a key in table 't' which has the value 'v'
+--------------------------------------------------------------------------------
 function VikingLibrary.GetKeyFromValue(t, value)
   for k,v in pairs(t) do
     if v==value then return k end
@@ -107,9 +118,45 @@ function VikingLibrary.GetKeyFromValue(t, value)
   return nil
 end
 
------------------------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+-- Number formating
+--
+-- Uses k instead of 1000. Ex: 27564 -> 27.5k
+--------------------------------------------------------------------------------
+function VikingLibrary:NumberToHuman(num)
+  if num ~= nil and num > 999 then
+    local decimalPlaces = 10^1
+    local roundedNum    = math.floor(num)
+    local sizes = {
+      { 'Billions', 1000000000 },
+      { 'Millions', 1000000 },
+      { 'Short', 1000 },
+    }
+
+    for i, value in ipairs(sizes) do
+      local abbrev = value[1]
+      local size = value[2]
+      if roundedNum >= size then
+        local number = math.floor(roundedNum * decimalPlaces / size) / decimalPlaces
+        local sType  = number % 1 ~= 0 and "Float" or "Whole"
+        local sFormatString = Apollo.GetString('TargetFrame_' .. abbrev .. 'Number' .. sType)
+
+        return String_GetWeaselString(sFormatString, number)
+      end
+    end
+  end
+  return num
+end
+
+--------------------------------------------------------------------------------
+-- Public Methods: END
+--------------------------------------------------------------------------------
+
+
+--------------------------------------------------------------------------------
 -- VikingLibrary OnDocLoaded
------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 function VikingLibrary:OnDocLoaded()
 
   if self.xmlDoc ~= nil and self.xmlDoc:IsLoaded() then
@@ -129,6 +176,10 @@ function VikingLibrary:OnDocLoaded()
   end
 end
 
+
+--------------------------------------------------------------------------------
+--
+--------------------------------------------------------------------------------
 function VikingLibrary.Include(modules)
 
   local m = {}
@@ -140,8 +191,8 @@ function VikingLibrary.Include(modules)
 end
 
 
------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- VikingLibrary Instance
------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 local VikingLibraryInst = VikingLibrary:new()
 VikingLibraryInst:Init()
